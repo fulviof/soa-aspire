a61eb93547d586312109e5f8d416910afc43d321
Fulvio Fanelli <70142915+fulviofanelli-unoeste@users.noreply.github.com>
2024-03-02 11:43:49 -0300

    teste

--- WebApis.Vestibular/Controllers/ProcessoSeletivoController.cs
+++ WebApis.Vestibular/Controllers/ProcessoSeletivoController.cs
@@ -10,6 +10,8 @@ namespace WebApis.Vestibular.Controllers
         [HttpGet]
         public IActionResult ObterProcessoSeletivo()
         {
+
+            //alteração vestibular
             return Ok(new List<object>() { 
                 new { nome = "Processo Seletivo Top Cursos Prudente", inicio = "26/02/2024", fim = "26/03/2024" },
                 new { nome = "Processo Seletivo Top Cursos Jaú", inicio = "26/02/2024", fim = "26/03/2024" },
