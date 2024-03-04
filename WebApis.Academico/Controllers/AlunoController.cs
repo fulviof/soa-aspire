817ba8b1af38a1849a6aa629af27ef5fa92b0f1c
Fulvio Fanelli <70142915+fulviofanelli-unoeste@users.noreply.github.com>
2024-03-04 10:44:52 -0300

    Update AlunoController.cs

--- WebApis.Academico/Controllers/AlunoController.cs
+++ WebApis.Academico/Controllers/AlunoController.cs
@@ -9,7 +9,7 @@ namespace WebApis.Academico.Controllers
         [HttpGet]
         public IActionResult RetornaAluno()
         {
-            //alteração academico3////asdasdasdasd
+            //alteração academico3////asdasdasdasdalterandoooooo
             return Ok(new {nome = "Fulvio Fanelli", curso =  "Sistemas de Informação", status = "Cursando"});
         }
     }
